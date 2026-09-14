2a7327b8f940c39b9c944e7333cdd60e147e86bb
Marius Biaou <mariusbiaou75@gmail.com>
2026-09-14 20:38:18 +0100

    fix(mail): encodage RFC 2047 MIME et headers From complets pour eviter le filtre anti-spam

--- app/Mail/PhpMailTransport.php
+++ app/Mail/PhpMailTransport.php
@@ -22,32 +22,46 @@ class PhpMailTransport extends AbstractTransport
             $email = MessageConverter::toEmail($rawMessage);
         }
 
-        $subject = $email->getSubject() ?? '';
+        $fromAddress = config('mail.from.address', 'bonjour@kimboo.net');
+        $fromName = config('mail.from.name', 'Kimboo');
+
+        $from = $email->getFrom();
+        if (!empty($from)) {
+            $fromHeader = $from[0]->toString();
+            $senderEmail = $from[0]->getAddress();
+        } else {
+            $fromHeader = "{$fromName} <{$fromAddress}>";
+            $senderEmail = $fromAddress;
+        }
+
+        $subject = $email->getSubject() ?? 'Notification Kimboo';
+        $encodedSubject = '=?UTF-8?B?' . base64_encode($subject) . '?=';
+
         $htmlBody = $email->getHtmlBody();
         $textBody = $email->getTextBody();
 
         $headers = [];
-        $from = $email->getFrom();
-        if (!empty($from)) {
-            $headers[] = 'From: ' . $from[0]->toString();
-            $headers[] = 'Reply-To: ' . $from[0]->toString();
-        }
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = "From: {$fromHeader}";
+        $headers[] = "Reply-To: {$fromHeader}";
+        $headers[] = "X-Mailer: PHP/" . phpversion();
 
         if ($htmlBody) {
-            $headers[] = 'MIME-Version: 1.0';
             $headers[] = 'Content-Type: text/html; charset=UTF-8';
+            $headers[] = 'Content-Transfer-Encoding: 8bit';
             $body = is_resource($htmlBody) ? stream_get_contents($htmlBody) : (string) $htmlBody;
         } else {
             $headers[] = 'Content-Type: text/plain; charset=UTF-8';
+            $headers[] = 'Content-Transfer-Encoding: 8bit';
             $body = is_resource($textBody) ? stream_get_contents($textBody) : (string) $textBody;
         }
 
-        $extraParams = "-f" . escapeshellarg($sender);
+        $extraParams = "-f" . escapeshellarg($senderEmail ?: $sender);
         $headerStr = implode("\r\n", $headers);
 
-        $success = @mail($recipients, $subject, $body, $headerStr, $extraParams);
+        $success = @mail($recipients, $encodedSubject, $body, $headerStr, $extraParams);
         if (!$success) {
-            $success = @mail($recipients, $subject, $body, $headerStr);
+            $success = @mail($recipients, $encodedSubject, $body, $headerStr);
         }
 
         if (!$success) {
