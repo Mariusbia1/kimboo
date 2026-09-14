4a64e30fae60a65996f4df8e6b8d6998b2499075
Marius Biaou <mariusbiaou75@gmail.com>
2026-09-14 20:25:04 +0100

    fix(mail): configuration sendmail -t -i pour compatibilite OVH mutualise

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,11 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('mail:test {email=bonjour@kimboo.net}', function ($email) {
+    $mailer = config('mail.default');
+    $this->info("Mailer actuel : {$mailer}");
+    if ($mailer === 'sendmail') {
+        $this->info("Sendmail path : " . config('mail.mailers.sendmail.path'));
+    }
     $this->info("Envoi d'un email de test vers {$email}...");
     try {
         Mail::raw("Ceci est un email de test Kimboo envoye depuis le serveur.", function ($message) use ($email) {
