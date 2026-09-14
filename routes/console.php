161d21ce5a0588370dd257edcf7b6869db1fbe07
Marius Biaou <mariusbiaou75@gmail.com>
2026-09-14 20:21:11 +0100

    feat: ajout commande artisan mail:test pour verification messagerie

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,20 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Mail;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('mail:test {email=bonjour@kimboo.net}', function ($email) {
+    $this->info("Envoi d'un email de test vers {$email}...");
+    try {
+        Mail::raw("Ceci est un email de test Kimboo envoye depuis le serveur.", function ($message) use ($email) {
+            $message->to($email)->subject("Test Messagerie Kimboo");
+        });
+        $this->info(">>> EMAIL ENVOYE AVEC SUCCES A {$email} <<<");
+    } catch (\Throwable $e) {
+        $this->error("Erreur lors de l'envoi : " . $e->getMessage());
+    }
+})->purpose('Tester l\'envoi d\'un email');
